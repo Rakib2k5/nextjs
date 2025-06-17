a8a66cbb4555470d2648708074290ccd3c5f1be5
Rakib2k5 <117804129+Rakib2k5@users.noreply.github.com>
2025-06-17 18:28:20 +0600

    Initialize pr-test/depends-on

---
+++ app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-gradient-defang">
+      <h1 className="text-4xl font-bold text-white">Next.js &times; DefangA</h1>
+    </main>
+  );
+}
